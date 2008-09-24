eaed66ca50afc8c6b04a9aa472ca00cb41d711de
Joachim Falk <falk@cs.fau.de>
2008-09-24 14:16:21 +0000

    * foreach-sources-in.sh also ignore dustbin directory
    git-archimport-id: falk@cs.fau.de--2008-systemoc-intern/scripts--mainline--0.1--patch-3

--- forall-sources-in.sh
+++ forall-sources-in.sh
@@ -3,7 +3,7 @@
 COMMAND=$1; shift
 
 SRCS=`find $@ \
-        -type d \( -name "{arch}" -o -name "obj" -o -name "dust-bin" \) -prune -o \
+        -type d \( -name "{arch}" -o -name "obj" -o -name "dust-bin" -o -name "dustbin" \) -prune -o \
         -type f \( -name "*.cpp" -o -name "*.tcpp" -o -name "*.hpp" -o -name "*.c" -o -name "*.h" \) -print`
 
 ${COMMAND} ${SRCS}
