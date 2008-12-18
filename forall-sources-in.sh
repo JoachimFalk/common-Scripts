ada959106ff848726831617212b4e2a9caf0cf23
Joachim Falk <falk@cs.fau.de>
2008-12-18 10:05:16 +0000

    git-archimport-id: falk@cs.fau.de--2008-systemoc-intern/scripts--mainline--0.1--patch-4

--- forall-sources-in.sh
+++ forall-sources-in.sh
@@ -4,6 +4,6 @@ COMMAND=$1; shift
 
 SRCS=`find $@ \
         -type d \( -name "{arch}" -o -name "obj" -o -name "dust-bin" -o -name "dustbin" \) -prune -o \
-        -type f \( -name "*.cpp" -o -name "*.tcpp" -o -name "*.hpp" -o -name "*.c" -o -name "*.h" \) -print`
+        -type f \( -name "*.cpp" -o -name "*.tcpp" -o -name "*.hpp" -o -name "*.C" -o -name "*.c" -o -name "*.h" \) -print`
 
 ${COMMAND} ${SRCS}
