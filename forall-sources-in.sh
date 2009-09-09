f3fedd12cf8ad47ab8c6b528268f9ceb905377b0
Joachim Falk <falk@cs.fau.de>
2009-09-09 08:34:08 +0000

    * @forall-sources-in.sh cover some more languages and file extensions.
    git-archimport-id: falk@cs.fau.de--2008-systemoc-intern/scripts--mainline--0.1--patch-12

--- forall-sources-in.sh
+++ forall-sources-in.sh
@@ -4,6 +4,13 @@ COMMAND=$1; shift
 
 SRCS=`find $@ \
         -type d \( -name "{arch}" -o -name "obj" -o -name "dust-bin" -o -name "dustbin" \) -prune -o \
-        -type f \( -name "*.cpp" -o -name "*.tcpp" -o -name "*.hpp" -o -name "*.C" -o -name "*.c" -o -name "*.h" \) -print`
+        -type f \( -name "*.cpp" -o -name "*.tcpp" -o -name "*.hpp" -o -name "*.thpp" -o \
+                   -name "*.cxx" -o -name "*.tcxx" -o -name "*.hxx" -o -name "*.thxx" -o -name "*.C" -o \
+                   -name "*.c" -o -name "*.h" -o \
+                   -name "*.perl" -o -name "*.pm" -o \
+                   -name "*.py" -o \
+                   -name "*.m4" -o \
+                   -name "*.sh" \
+                \) -print`
 
 ${COMMAND} ${SRCS}
