c8c613c2f9667a7e83d3d4bc34f888b25afe9364
Joachim Falk <falk@cs.fau.de>
2010-01-19 13:06:14 +0000

    * Modified forall-sources-in.sh to exclude aclocal.m4.
    git-archimport-id: falk@cs.fau.de--2008-systemoc-intern/scripts--mainline--0.1--patch-15

--- forall-sources-in.sh
+++ forall-sources-in.sh
@@ -4,6 +4,7 @@ COMMAND=$1; shift
 
 SRCS=`find $@ \
         -type d \( -name "{arch}" -o -name "obj" -o -name "dust-bin" -o -name "dustbin" \) -prune -o \
+        -type f \( -name "aclocal.m4" \) -prune -o \
         -type f \( -name "*.cpp" -o -name "*.tcpp" -o -name "*.hpp" -o -name "*.thpp" -o \
                    -name "*.cxx" -o -name "*.tcxx" -o -name "*.hxx" -o -name "*.thxx" -o -name "*.C" -o \
                    -name "*.c" -o -name "*.h" -o \
