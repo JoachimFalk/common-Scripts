1fe80728e3a5473622d31d2b79a02943f1d082f7
Joachim Falk <falk@cs.fau.de>
2010-04-19 11:03:58 +0200

    Added git support to forall-projects.sh.

--- forall-projects.sh
+++ forall-projects.sh
@@ -7,11 +7,15 @@ EXEC="$@"
 
 if test x"$1" = x"tla"; then
   EXEC="$EXEC | grep -v '^\*'"
+elif test x"$1" = x"git"; then
+  EXEC="$EXEC | grep -v '^# Not currently on any branch\.'"
 fi
 
+# GNU arch (tla) stuff
+
 check_config() {
-  BASE=$1;
-  CONFIG=$2;
+  local BASE=$1;
+  local CONFIG=$2;
   cat $BASE/$CONFIG | \
     sed -e 's/[ 	]*#.*$//' -e '/^$/d' | \
     while read dir arch; do \
@@ -31,8 +35,8 @@ check_config() {
 }
 
 test_config() {
-  BASE=$1;
-  CONFIG=$2;
+  local BASE=$1;
+  local CONFIG=$2;
   if ! sed -e 's/[ 	]*#.*$//' -e '/^$/d' $BASE/$CONFIG | \
     grep -v '^[a-zA-Z0-9+./_-]\+[ 	]\+[^@]\+@[^@]\+--\([a-zA-Z0-9_]\|-[a-zA-Z0-9_]\)\+/\([a-zA-Z0-9_]\|-[a-zA-Z0-9_]\)\+--\(\([a-zA-Z0-9_]\|-[a-zA-Z0-9_]\)\+--\)\?\([a-zA-Z0-9_]\|-[a-zA-Z0-9_]\)\+' > /dev/null; then
     return 0;
@@ -41,17 +45,65 @@ test_config() {
   fi
 }
 
-test -d $BASE && ( cd $BASE; echo "# .	`tla tree-id`"; eval $EXEC );
+# git stuff
 
-find $BASE -type d \( -name "{arch}" -o -name ".git" \) -prune -o \
-  -type f \( -name "config-docu" -o -name "config" \) -print | \
-  while read file; do \
-    test -f $file && ( \
-    dir=`dirname $file`; \
-    config=`basename $file`; \
-    if test_config $dir $config; then
-      check_config $dir $config;
+git_recurse() {
+  local BASE=$1;
+  ( cd "$BASE" && eval $EXEC )
+  echo
+  ( cd "$BASE" && git submodule ) | \
+    while read subline; do
+      case "$subline" in
+        "+"*)
+          # Is dirty
+          STATUS="+"
+          subline="${subline#?}"
+          ;;
+        *)
+          # Is clean
+          STATUS=" "
+          ;;
+      esac
+      HASH="${subline%% *}"; subline="${subline#* }"
+      DIR="${subline%% *}";  subline="${subline#* (}"
+      SYM="${subline%)*}"
+      echo "### ${STATUS}${HASH} $BASE/$DIR (${SYM})"
+      git_recurse "$BASE/$DIR"
+    done
+}
+
+if test -d "$BASE/.git"; then
+  # Do git stuff
+  ( cd $BASE;
+    if test x"`git status -s`" = x""; then
+      # Is clean
+      STATUS=" "
     else
-      echo "Skipping non-arch config: $file"
-    fi;)
-  done
+      # Is dirty
+      STATUS="+"
+    fi
+    # Get the hash
+    HASH=`git show --pretty=oneline --shortstat HEAD | sed -ne '1,1{s/ .*//;p}'`
+    # Get the symbolic name
+    SYM=`git describe --all`
+    echo "### ${STATUS}${HASH} $BASE (${SYM})"
+    eval $EXEC 
+  )
+  git_recurse "$BASE"
+else
+  # Do tla stuff
+  test -d $BASE && ( cd $BASE; echo "# .	`tla tree-id`"; eval $EXEC );
+  
+  find $BASE -type d \( -name "{arch}" -o -name ".git" \) -prune -o \
+    -type f \( -name "config-docu" -o -name "config" \) -print | \
+    while read file; do \
+      test -f $file && ( \
+      dir=`dirname $file`; \
+      config=`basename $file`; \
+      if test_config $dir $config; then
+        check_config $dir $config;
+      else
+        echo "Skipping non-arch config: $file"
+      fi;)
+    done
+fi
